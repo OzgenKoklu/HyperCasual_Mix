58ce8c81f0420a01abc5686a1e9e988371dc2e11
Ozgen Koklu <ozgen.koklu@gmail.com>
2022-06-10 13:50:53 +0300

    added manager scripts, singleton class etc.

--- Runner/Assets/Scripts/LevelManager.cs
+++ Runner/Assets/Scripts/LevelManager.cs
@@ -1,16 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour
+public class LevelManager : Singleton<LevelManager>
 {
+    [Header("Level System")]
+    private int whichlevel = 0;
+    [SerializeField] public List<ScriptableObject> levels = new List<ScriptableObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Initialize();
     }
 
+    private void Initialize()
+    {
+        whichlevel = PlayerPrefs.GetInt("whichlevel");
+
+        if (PlayerPrefs.GetInt("randomlevel") > 0)
+        {
+            whichlevel = Random.Range(0, levels.Count);
+        }
+
+        Instantiate(levels[whichlevel].levelPrefab, Vector3.zero, Quaternion.identity);
+    }
+
+    public void NextLevel()
+    {
+        whichlevel++;
+        PlayerPrefs.SetInt("whichlevel", whichlevel);
+        if(whichlevel >= levels.Count)
+        {
+            whichlevel--;
+            PlayerPrefs.SetInt("randomlevel", 1);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
     // Update is called once per frame
+
+    public int WhichLevel()
+    {
+        return whichlevel;
+    }
     void Update()
     {
         
